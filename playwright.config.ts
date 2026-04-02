a9ca9c263f1849624151621b030b3a643773906a
Alexlee2046 <alexlee2046@users.noreply.github.com>
2026-04-02 20:43:25 +0800

    chore: switch playwright config from firefox to chromium for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ export default defineConfig({
     headless: true,
   },
   projects: [
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
     },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ],
 });
